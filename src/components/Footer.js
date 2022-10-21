8d0b38570c26974c6a56d944a62d631da9408e30
EmmaDwebber <oladejiemmanuel2016@gmail.com>
2022-10-21 10:00:23 -0300

    First commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+ 
+ export default function Footer(){
+     return(
+          <footer>
+                <img src="./images/FacebookIcon.png" alt="twitter icon" className="facebook" />
+                <img src="./images/GitHubIcon.png" alt="Github icon"/>
+                <img src="./images/InstagramIcon.png" alt="Instagram icon" />
+                <img src="./images/Linkedin.png" alt="Instagram icon" />
+                <img src="./images/TwitterIcon.png" alt="Twitter icon"  />
+        </footer>
+     )
+ }           
+           
